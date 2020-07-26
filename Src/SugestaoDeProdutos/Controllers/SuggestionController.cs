329c3ff5a9ecc2c18b4802e13b8ed800ca3b7e0f
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2020-07-26 05:40:12 -0300

    Add swagger

---
+++ Src/SugestaoDeProdutos/Controllers/SuggestionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SugestaoDeProdutos.Controllers
+{
+    [Produces("application/json")]
+    [Route("")]
+    [ApiController]
+    public class SuggestionController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult PostSuggestionAsync()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Src/SugestaoDeProdutos/Startup.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SugestaoDeProdutos
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// <value>The configuration.</value>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="env">The env.</param>
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddUserSecrets<Startup>()
+                .AddEnvironmentVariables()
+                .AddCommandLine(Environment.GetCommandLineArgs());
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v2",
+                    new OpenApiInfo
+                    {
+                        Title = "Sugestão de produtos",
+                        Version = "v2",
+                        Description = "API de sugestão de produtos da Editora Inovação / Inovação Media Brasil",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Guilherme Branco Stracini",
+                            Url = new Uri("https://github.com/guibranco")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Use under MIT",
+                            Url = new Uri("https://github.com/InovacaoMediaBrasil/SugestaoDeProdutos/blob/master/LICENSE"),
+                        }
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Sugestão de Produtos v2");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
